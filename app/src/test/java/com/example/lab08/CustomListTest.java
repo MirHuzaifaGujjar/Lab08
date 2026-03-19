810c7b3dd0885ba03681e8bc10feaf4d083267cd
MirHuzaifaGujjar <mirhuzaifa230@gmail.com>
2026-03-19 16:31:23 +0500

    deletecity test but no function

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -11,4 +11,14 @@ public class CustomListTest {
         // This will fail initially because hasCity() doesn't exist
         assertTrue(list.hasCity(calgary));
     }
+
+    @Test
+    public void testDeleteCity() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+        list.deleteCity(calgary);
+        // This will fail initially because deleteCity() doesn't exist yet
+        assertFalse(list.hasCity(calgary)); // After deleting, the city should no longer be in the list
+    }
 }
